e7fa9e74c1fc573c385dfddcad9f8f23b1609c59
sean.kerr <sean.kerr@S100470DRAFTING.mccd.edu>
2026-09-10 13:57:25 -0700

    Finished code. Added a bunch of stuff. Final product etc.

--- src/Course/CourseTester.java
+++ src/Course/CourseTester.java
@@ -3,22 +3,76 @@
 //My object test class
 package Course;
 
+import java.util.Scanner;
+
 public class CourseTester {
 
 	public static void main(String[] args) {
-		Course course1 = new Course();
-
-		System.out.println("Printing course1 directly:");
+		Scanner scnr = new Scanner(System.in);
+		Course course1 = new Course();	
+		System.out.println("\nDefaults for Course1 fields (before any assignment):");
 		System.out.println(course1.toString());
 
-		System.out.println("\nDefaults for Course1 fields (before any assignment):");
+		
 
 		Course course2 = new Course();
-
-		System.out.println("Printing course1 directly:");
+		
+		System.out.println("\nPlease type in the name of the Class:");
+		String NAME = scnr.nextLine();
+		course2.setName(NAME);
+		
+		System.out.println("\nPlease type a grade of the variety [A, B, C, D, E, F, N/A]");
+		String GRADE = scnr.nextLine();
+		Grade gradeEntered;
+		if (GRADE.equalsIgnoreCase("N/A")) {
+			gradeEntered = Grade.NOT_ASSIGNED;
+		}
+		else {
+			gradeEntered = Grade.valueOf(GRADE.toUpperCase());
+		}
+		course2.setGrade(gradeEntered);
+		
+		System.out.println("\nPlease enter classroom number:");
+		String NUMBER = scnr.nextLine();
+		course2.setClassroom(NUMBER);
+		
+		
+		
+		System.out.println("\nPrinting course1 directly:");
 		System.out.println(course2.toString());
+		
+		System.out.println("\nWould you like to add another class?");
+		System.out.println("Type yes or no, first letters of words are also acceptable");
+		String response = scnr.nextLine();
+		char choice = Character.toLowerCase(response.charAt(0));
+		while (choice == 'y') {
+			Course course3 = new Course();
+			System.out.println("\nPlease type in the name of the Class:");
+			String NAME2 = scnr.nextLine();
+			course3.setName(NAME2);
+			
+			System.out.println("\nPlease type a grade of the variety [A, B, C, D, E, F, N/A]");
+			String GRADE2 = scnr.nextLine();
+			Grade gradeEntered2;
+			if (GRADE2.equalsIgnoreCase("N/A")) {
+				gradeEntered2 = Grade.NOT_ASSIGNED;
+			}
+			else {
+				gradeEntered2 = Grade.valueOf(GRADE2.toUpperCase());
+			}
+			course2.setGrade(gradeEntered2);
+			
+			System.out.println("\nPlease enter classroom number:");
+			String NUMBER2 = scnr.nextLine();
+			course2.setClassroom(NUMBER2);
+			
+			  System.out.println("\nWould you like to add another class?");
+			    System.out.println("Type y or n");
+			    String response2 = scnr.nextLine();
+			    choice = Character.toLowerCase(response2.charAt(0));
+		}
+		
 
-		System.out.println("\nCourse2 fields (before any assignment):");
 
 	}
 
